04cd6673e41d47ce8797cf71b21976b01c130319
Atakan Akdıkan <32209119+aynstayn@users.noreply.github.com>
2017-11-02 11:47:23 +0300

    Add files via upload

--- donguler.java
+++ donguler.java
@@ -2,31 +2,107 @@ package ybsA;
 import java.util.Scanner;
 public class donguler {
 
-	public static void main(String[] args) {
+	 public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		/* ********************************************************************* 8.SORU
+Scanner input = new Scanner(System.in);
+System.out.println("Dakika giriniz: ");
+ long minutes = input.nextLong();
+ 
+ long numberOfDays = minutes / (24 * 60);
+ long numberOfYears = numberOfDays / 365;
+ long remaingDays = numberOfYears % 365;
+ 
+ System.out.println(minutes + " dakika yaklasik " + numberOfYears + " sene ve " + remaingDays + " gundur." );
+*/
 
-		// veri yarat
-			int num1, num2;
-			num1 = (int) (Math.random() * 10);
-			num2 = (int) (Math.random() * 10);
-			int cevap;
-			int sorgusonucu;
-			System.out.println("num1 =" + num1 + "num2 =" + num2 );
+	/*
+		
+		//********************************************************************** 9.SORU
+		Scanner input = new Scanner(System.in);
+		int x1, x2, y1, y2;
+		System.out.println("lokasyon gir -x1");
+		x1 = input.nextInt();
+		System.out.println("lokasyon gir -x2");
+		x2 = input.nextInt();
+		System.out.println("lokasyon gir -y1");
+		y1 = input.nextInt();
+		System.out.println("lokasyon gir -y2");
+		y2 = input.nextInt();
+		int distance = (int) Math.pow( (x2- x1) * (x2-x1) + (y2 - y1) * (y2 - y1), 0.5); 
+		System.out.println(distance);
+	
+	*/
+	
+		//10.Soru
+		
+		
+		
+	
+		//************************************************************************* 12.soru
+		/*
+		int day, daycase;
+		Scanner input = new Scanner (System.in);
+		System.out.println("0-6 arasý bir sayý giriniz: ");
+		day = input.nextInt();
+		switch (day){
+		case 0:
+			System.out.println("Pazar"); break;
+		case 1:
+			System.out.println("Pazartesi"); break;
+		case 2:
+			System.out.println("sali"); break;
+		case 3:
+			System.out.println("carsamba"); break;
+		case 4:
+			System.out.println("persembe"); break;
+		case 5:
+			System.out.println("Cuma"); break;
+		case 6:
+			System.out.println("Cumartesi"); break;
 			
-			
-			// kullanicidan cevap al
-			Scanner input = new Scanner(System.in);
-			System.out.println("iki sayinin toplamini giriniz: ");
-			cevap = input.nextInt();
-			
-			// cevaba baglý mesaj göster:
-			sorgusonucu = num1+num2;
-			 while (sorgusonucu != cevap) {
-				System.out.println("Yanlis cevap tekrar giriniz");
-				cevap = input.nextInt();
-				
-			 }
-			 System.out.println("Dogru cevap");
-
+			*/
+		/*
+		//********************************************************************** 13.Soru:
+		int rastgelesayi, kullsayi, toplam, ondalik, birlik, yuzluk;
+		rastgelesayi = (int) (Math.random() *1000);
+		System.out.println("sayilarin toplamini giriniz: " + rastgelesayi );
+		Scanner input = new Scanner(System.in);
+		kullsayi = input.nextInt();
+		yuzluk = rastgelesayi / 100;
+		ondalik = (rastgelesayi % 100) / 10;
+		birlik = ( ondalik % 10) / 1;
+		toplam = yuzluk + ondalik + birlik;
+		 if (toplam == kullsayi)
+			 System.out.println("Dogru");
+		 else 
+			 System.out.println("Yanlis");
+		*/
+		 //********************************************************************* 14.Soru
+		 /*
+		 Scanner input = new Scanner(System.in);
+		 
+		 
+		 System.out.print("Uc haneli sayi gir: ");
+		 int number = input.nextInt();
+		 
+		 if (number /100 == number %10)
+			 System.out.println(number + " is a palindrome");
+		 else 
+			 System.out.println(number + " is not a polindrome");
+		 
+	*/ 
+		
+		 	
+		 				// palindronme sorusu önemli 
+		 				//********* mil sorusu alt alta olacak / 15.soru
+			 			// kareköklü soru
+		 				// 14.soru daire algoritma verilecek java diline çevir.
+		 				// 17 olabilir. ORNEK SORULARA BAK......********* 
+		 
+		     
+		
 }
 }
